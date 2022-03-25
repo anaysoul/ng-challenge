b8264f2c48256f3dc8bda3ef57d6b7ae4349d1fa
anaysoul <soulyana_lakew@outlook.com>
2022-03-25 15:04:52 -0400

    built and styled card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'challenge';
+  title = 'Typing Challenge';
+  text = 'Lorem ipsum leo risus';
+
+  @Input() inputText: string = '';
 }
